6ee3724601a955bc28b50a4d13986fdda254c72d
chaithanya <chaithanya1812@gmail.com>
2024-12-08 19:57:21 +0530

    sample werf

--- load-schema/run.sh
+++ load-schema/run.sh
@@ -1,13 +1,26 @@
+if [ -v db_type ]; then  echo  "** db_type ** variable is missing" && exit 1 ; fi
+
 if [ "${db_type}" == "mysql" ];
 then
+    if [[ -v mysql_host &&  -v mysql_username && -v -mysql_password ]] ; then
+      echo "** mysql_host ** check variables"
+      echo "** mysql_username ** check variables"
+      echo "** mysql_password ** check variables"
+      exit 1
+      fi
 git clone https://github.com/RoboShop1/r-shipping.git /app
 cd /app
 for i in `ls db`; do
 mysql -h ${mysql_host} -u${mysql_username} -p${mysql_password} < /app/db/${$i}.sql
 done
 
+
 if [ "${db_type}" == "mongo" ];
 then
+    if [[ -v mongo_host ]] ; then
+      echo "** mongo_host ** check variables"
+      exit 1
+      fi
 git clone https://github.com/RoboShop1/r-catalogue.git /app
 cd /app
 for i in `ls db`; do
