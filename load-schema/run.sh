29aa04c41a66f1359f5b1c336f2c8724b525e2f4
chaithanya <chaithanya1812@gmail.com>
2024-12-09 22:27:30 +0530

    sample werf1

--- load-schema/run.sh
+++ load-schema/run.sh
@@ -11,17 +11,14 @@ then
       exit 1
       fi
 git clone https://github.com/RoboShop1/r-shipping.git /app
+
 cd /app
-sleep 1000
-echo "mysql -h ${mysql_host} -u${mysql_username} -p${mysql_password} < db/app-user.sql"
+
+mysql -h ${mysql_host} -u${mysql_username} -p${mysql_password} < db/schema.sql
+
 mysql -h ${mysql_host} -u${mysql_username} -p${mysql_password} < db/app-user.sql
 
-
-echo "mysql -h ${mysql_host} -u${mysql_username} -p${mysql_password} < db/master-data.sql"
 mysql -h ${mysql_host} -u${mysql_username} -p${mysql_password} < db/master-data.sql
-
-echo "mysql -h ${mysql_host} -u${mysql_username} -p${mysql_password} < db/schema.sql"
-mysql -h ${mysql_host} -u${mysql_username} -p${mysql_password} < db/schema.sql
 
 fi
 
